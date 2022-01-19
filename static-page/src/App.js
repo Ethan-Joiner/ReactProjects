6c1bdc5dd9be0f5c4ef549d26d413e11090281aa
Ethan-Joiner <ethan.joiner@gmail.com>
2022-01-19 12:02:42 -0600

    tedious

--- static-page/src/App.js
+++ static-page/src/App.js
@@ -5,14 +5,10 @@ function App() {
   return (
     <div>
         <img src={logo} className = 'App-logo' alt="logo" />
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>  
+        <ol>
+          <li>Item One</li>
+          <li>Item Two</li>
+        </ol>
     </div>
   );
 }
